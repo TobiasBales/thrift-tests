d1b84382f9b2deded732b6709f3e6963f24ab7c5
Tobias Bales <tobias.bales@gmail.com>
2018-11-03 21:49:58 +0100

    Initial version

---
+++ src/main/thrift/api.thrift
@@ -0,0 +1,16 @@
+struct TestRequest {
+    1: optional string uri
+}
+
+struct TestResponse {
+}
+
+exception TestException {}
+
+service Test {
+    TestResponse test(
+        1: TestRequest request
+    ) throws (
+        1: TestException e
+    )
+}
